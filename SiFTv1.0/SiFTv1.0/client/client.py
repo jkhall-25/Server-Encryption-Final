0c8d6f48e36048e8be39c7fd4c205b42495f1dbd
Jules <jkhall-25@outlook.com>
2025-01-02 11:34:53 -0600

    Finalize protocol and confirm to-spec performance

--- SiFTv1.0/SiFTv1.0/client/client.py
+++ SiFTv1.0/SiFTv1.0/client/client.py
@@ -10,8 +10,8 @@ from siftprotocols.siftupl import SiFT_UPL, SiFT_UPL_Error
 from siftprotocols.siftdnl import SiFT_DNL, SiFT_DNL_Error
 
 # ----------- CONFIG -------------
-#server_ip = '127.0.0.1' # localhost 
-server_ip = '192.168.20.24'
+server_ip = '127.0.0.1' # localhost 
+#server_ip = '192....'
 server_port = 5150
 # --------------------------------
 
